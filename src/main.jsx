cfd25af2ce1e7542baad9ee08522182827cd5839
Aatissh Vijay <aatisshvijay@Aatisshs-MacBook-Air.local>
2025-10-05 13:35:46 +0530

    new

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Analytics } from '@vercel/analytics' 
+import { Analytics } from '@vercel/analytics/react' // <-- Here
 
 import React from 'react'
 
